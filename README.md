ceed9b410aca0b7dc4291af0804caf6bc04f3b74
PMJ <pmj@pmj.hu>
2021-03-14 21:43:42 +0100

    Conditional target selection

--- README.md
+++ README.md
@@ -1,5 +1,28 @@
 # Usage
 
+## Build for Windows x64
+
 ```
-docker run --rm -v `pwd`:/build pmjohann/crossnode
+docker run --rm -v `pwd`:/build pmjohann/crossnode windows
+```
+
+## Build for Darwin x64
+
+```
+docker run --rm -v `pwd`:/build pmjohann/crossnode darwin
+```
+
+## Build for Linux x64
+
+```
+docker run --rm -v `pwd`:/build pmjohann/crossnode linux
+```
+
+## Build for multiple platforms at once
+
+You can include multiple targets separated by comma.
+Make sure not to use spaces between them!
+
+```
+docker run --rm -v `pwd`:/build pmjohann/crossnode windows,linux,mac
 ```
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -9,6 +9,15 @@ rm -rf /cleanbuild/node_modules
 # INSTALL PACKAGE DEPENDENCIES
 npm install
 
-nexe -i "/cleanbuild/$ENTRYPOINT" -t "windows-x64-$NODE_VERSION" -o "/build/win64.exe"
-nexe -i "/cleanbuild/$ENTRYPOINT" -t "mac-x64-$NODE_VERSION" -o "/build/darwin64"
-nexe -i "/cleanbuild/$ENTRYPOINT" -t "linux-x64-$NODE_VERSION" -o "/build/linux64"
+# CONDITIONAL BUILDS
+if echo $1 | grep "windows" > /dev/null; then
+    nexe -i "/cleanbuild/$ENTRYPOINT" -t "windows-x64-$NODE_VERSION" -o "/build/win64.exe"
+fi
+
+if echo $1 | grep "darwin" > /dev/null; then
+    nexe -i "/cleanbuild/$ENTRYPOINT" -t "mac-x64-$NODE_VERSION" -o "/build/darwin64"
+fi
+
+if echo $1 | grep "linux" > /dev/null; then
+    nexe -i "/cleanbuild/$ENTRYPOINT" -t "linux-x64-$NODE_VERSION" -o "/build/linux64"
+fi
