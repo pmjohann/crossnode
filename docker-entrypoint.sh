1b9059e95fbf43a94f27660a873d2053f72eb614
PMJ <pmj@pmj.hu>
2021-03-14 21:31:11 +0100

    Build in clean build folder

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
+# COPY BIND MOUNTED BUILD FOLDER TO A CLEAN PLACE
+cp -R /build /cleanbuild && cd /cleanbuild
+
+# REMOVE node_modules FOLDER
+rm -rf /cleanbuild/node_modules
+
+# INSTALL PACKAGE DEPENDENCIES
 npm install
 
-nexe -i "/build/$ENTRYPOINT" -t "windows-x64-$NODE_VERSION" -o "/build/win64.exe"
-nexe -i "/build/$ENTRYPOINT" -t "mac-x64-$NODE_VERSION" -o "/build/darwin64"
-nexe -i "/build/$ENTRYPOINT" -t "linux-x86-$NODE_VERSION" -o "/build/linux64"
+nexe -i "/cleanbuild/$ENTRYPOINT" -t "windows-x64-$NODE_VERSION" -o "/build/win64.exe"
+nexe -i "/cleanbuild/$ENTRYPOINT" -t "mac-x64-$NODE_VERSION" -o "/build/darwin64"
+nexe -i "/cleanbuild/$ENTRYPOINT" -t "linux-x64-$NODE_VERSION" -o "/build/linux64"
